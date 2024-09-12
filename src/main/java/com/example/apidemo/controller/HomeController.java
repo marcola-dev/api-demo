ba2d17d82cdbd48fc80f88b6ea0aad690fb1f4c7
scriptored <marcois.eduardo072002@gmail.com>
2024-09-12 19:00:15 -0300

    add > src/

---
+++ src/main/java/com/example/apidemo/controller/HomeController.java
@@ -0,0 +1,23 @@
+package com.example.apidemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String redirectToLogin() {
+        return "redirect:/login";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
